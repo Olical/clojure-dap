2691b160303cbe1576f84cc4216e1ae2a66b7f9b
Oliver Caldwell <ollie@oli.me.uk>
2023-01-22 15:34:03 +0000

    Add some todos and change the hello world println

--- src/clojure_dap/main.clj
+++ src/clojure_dap/main.clj
@@ -1,6 +1,11 @@
 (ns clojure-dap.main
   (:require [clojure.pprint :as pp]))
 
+;; TODO core.async or manifold + claypoole
+;; TODO DAP IO layer (can read from stdio or a channel)
+;; TODO nREPL IO layer (can write to an nREPL or channel)
+;; TODO System in the middle that can talk to both side's channels
+
 (defn run [opts]
   (pp/pprint opts)
-  (println "Hello, Twitch and YouTube!"))
+  (println "Hello, World!"))
