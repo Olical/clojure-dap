3fddbe1ab03f71dae6e2ca8431e4e569d351b974
Oliver Caldwell <ollie@oli.me.uk>
2023-01-22 22:48:46 +0000

    Simplify main

--- src/clojure_dap/main.clj
+++ src/clojure_dap/main.clj
@@ -2,10 +2,6 @@
   "Entrypoint for the actual program, handles starting of systems and CLI input."
   (:require [clojure.pprint :as pp]))
 
-;; TODO DAP IO layer (can read from stdio or a channel)
-;; TODO nREPL IO layer (can write to an nREPL or channel)
-;; TODO System in the middle that can talk to both side's channels
-
 (defn run
   "CLI entrypoint to the program, boots the system and handles any CLI args."
   [opts]
--- test/clojure_dap/main_test.clj
+++ test/clojure_dap/main_test.clj
@@ -1,7 +1,1 @@
-(ns clojure-dap.main-test
-  (:require [clojure.test :as t]
-            [clojure-dap.main :as main]))
-
-(t/deftest hello
-  (t/testing "math still works"
-    (t/is (= 2 (+ 1 1)))))
+(ns clojure-dap.main-test)
