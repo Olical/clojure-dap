b983ebb8e4c6ce29b567faa114c5b4941f2022e8
Oliver Caldwell <ollie@oli.me.uk>
2023-01-22 22:49:36 +0000

    Initial server loops and exit system

--- src/clojure_dap/server.clj
+++ src/clojure_dap/server.clj
@@ -1,2 +1,32 @@
 (ns clojure-dap.server
-  "Core of the system, give it some IO to communicate with the client through and an nREPL server to drive from user inputs and it'll handle the rest. Understands both the DAP and nREPL messages, relaying ideas between the two.")
+  "Core of the system, give it some IO to communicate with the client through and an nREPL server to drive from user inputs and it'll handle the rest. Understands both the DAP and nREPL messages, relaying ideas between the two."
+  (:require [manifold.stream :as s]
+            [manifold.deferred :as d]))
+
+(defn start
+  "Creates a new server that contains a few processes. Returns a function that when called will stop the server."
+  [{:keys [client-io nrepl-io]}]
+  (let [stop-promise! (promise)
+        stop-fn (fn []
+                  (deliver stop-promise! ::stop)
+                  true)]
+
+    ;; Client read loop
+    (d/future
+      (loop []
+        (let [input @(d/alt stop-promise! (s/take! (:input client-io)))]
+          (println "===" input)
+          (if (= input ::stop)
+            ::stopped
+            (recur)))))
+
+    ;; nREPL read loop
+    (d/future
+      (loop []
+        (let [input @(d/alt stop-promise! (s/take! (:input nrepl-io)))]
+          (println "---" input)
+          (if (= input ::stop)
+            ::stopped
+            (recur)))))
+
+    stop-fn))
---
+++ test/clojure_dap/server_test.clj
@@ -0,0 +1,32 @@
+(ns clojure-dap.server-test
+  (:require [clojure.test :as t]
+            [manifold.stream :as s]
+            [clojure-dap.server :as server]
+            [clojure-dap.stream :as stream]))
+
+(defn start-test-server []
+  (let [opts {:client-io (stream/io)
+              :nrepl-io (stream/io)}]
+    (merge
+      opts
+      {:stop(server/start opts)})))
+
+(t/deftest start-stop
+  (t/testing "we can start and stop the system"
+    (let [{:keys [stop]} (start-test-server)]
+      (t/is (stop)))))
+
+;; -> initialise request
+;; <- capabilities
+
+;; <- initialised event
+
+;; ONE POSSIBILITY
+;; <- terminated event
+;; <- exited event
+
+;; Other exits:
+;; The terminate request is sent from the client to the debug adapter in order to shut down the debuggee gracefully. Clients should only call this request if the capability supportsTerminateRequest is true.
+;; The disconnect request asks the debug adapter to disconnect from the debuggee (thus ending the debug session) and then to shut down itself (the debug adapter). (which may also terminate if the opt is true!)
+
+; (t/deftest minimal-session)
