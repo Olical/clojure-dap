4172994fe02f0f9c423b2122e6e444667bbf9b77
Oliver Caldwell <oliver.caldwell@viasat.com>
2023-05-05 23:03:05 +0100

    Enable malli instrumentation during tests (omg yay)

---
+++ src/clojure_dap/kaocha_hooks.clj
@@ -0,0 +1,10 @@
+(ns clojure-dap.kaocha-hooks
+  "Custom hooks for Kaocha used to customise the test runner. See tests.edn for configuration."
+  (:require [malli.instrument :as mi]
+            [malli.dev.pretty :as malli-pretty]))
+
+(defn pre-run
+  "Run after the config is loaded and the tests are planned. Just before the tests are actually executed."
+  [test-plan]
+  (mi/instrument! {:report (malli-pretty/thrower)})
+  test-plan)
