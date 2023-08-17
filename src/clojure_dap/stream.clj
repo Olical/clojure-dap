d085ee264415809a21da9cac84db9105817fef98
Oliver Caldwell <ollie@oli.me.uk>
2023-08-17 22:06:52 +0100

    Got a simple header and message parsing

--- src/clojure_dap/stream.clj
+++ src/clojure_dap/stream.clj
@@ -1,8 +1,54 @@
 (ns clojure-dap.stream
-  (:require [manifold.stream :as s]))
+  "Tools to work with DAP streams and streams in general."
+  (:require [clojure.string :as str]
+            [de.otto.nom.core :as nom]
+            [jsonista.core :as json]
+            [cognitect.anomalies :as anom]
+            [manifold.stream :as s]))
 
 (defn io
   "Create an input/output stream pair. Input is coming towards your code, output is heading out from your code."
   []
   {:input (s/stream)
    :output (s/stream)})
+
+(defn parse-header
+  "Given a header string of the format 'Content-Length: 119\\r\\n\\r\\n' it returns a map containing the key value pairs."
+  [header]
+  (into
+   {}
+   (map
+    (fn [line]
+      (let [[k v] (str/split line #": " 2)]
+        [(keyword k) (json/read-value v)])))
+   (str/split-lines (str/trim-newline header))))
+
+(defn read-message
+  "Reads a DAP message from the input stream. Assumes a few things: The first character we're going to read will be the beginning of a new messages header AND the stream will consist of single characters.
+
+  It works by reading the header (Content-Length: 119\\r\\n\\r\\n) until a double \\r\\n\\r\\n at which point it knows the Content-Length and can read the rest of the message.
+
+  Once read, we decode the JSON body and validate it against the JSON schemas before returning the valid message or an anomaly.
+
+  Will block until a message is read!"
+  [input-stream]
+  (loop [header-buffer ""]
+    (let [next-char @(s/take! input-stream)]
+      (if (char? next-char)
+        (let [header-buffer (str header-buffer next-char)]
+          (if (str/ends-with? header-buffer "\r\n\r\n")
+            (let [{:keys [Content-Length]} (parse-header header-buffer)
+                  body (str/join @(s/take! (s/batch Content-Length input-stream)))]
+              (try
+                (json/read-value body json/keyword-keys-object-mapper)
+                (catch Exception e
+                  (nom/fail
+                   ::anom/incorrect
+                   {::anom/message "Failed to parse DAP message JSON"
+                    ::body body
+                    ::error (Throwable->map e)}))))
+            (recur header-buffer)))
+        (nom/fail
+         ::anom/incorrect
+         {::anom/message "Received a non-character while reading the next DAP message. A nil probably means the stream closed."
+          ::value next-char})))))
--- test/clojure_dap/stream_test.clj
+++ test/clojure_dap/stream_test.clj
@@ -8,3 +8,36 @@
     (let [{:keys [input output]} (stream/io)]
       (t/is (s/stream? input))
       (t/is (s/stream? output)))))
+
+(def example-message
+  "Content-Length: 112\r\n\r\n{
+    \"seq\": 153,
+    \"type\": \"request\",
+    \"command\": \"next\",
+    \"arguments\": {
+        \"threadId\": 3
+    }
+}")
+
+(t/deftest parse-header
+  (t/testing "simple header"
+    (t/is (= {:Content-Length 119}
+             (stream/parse-header
+              "Content-Length: 119\r\n\r\n")))))
+
+(t/deftest read-message
+  (t/testing "reads a DAP message from a input-stream"
+    (let [{:keys [input _output]} (stream/io)]
+
+      ;; Assumption! Messages are fed in character by character.
+      (s/put-all! input (char-array example-message))
+
+      (t/is (= {:seq 153
+                :type "request"
+                :command "next"
+                :arguments {:threadId 3}}
+               (stream/read-message input)))))
+
+  (t/testing "returns an anomaly if we get some bad input")
+  (t/testing "returns an anomaly if we can read a message but it's malformed")
+  (t/testing "returns an anomaly if the stream closes"))
