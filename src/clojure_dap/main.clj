05964d67fb89d378f00c6be9a8e6ccd7a3d377dc
Oliver Caldwell <ollie@oli.me.uk>
2023-09-11 21:00:41 +0100

    Start the server with stub IO and more logs

--- src/clojure_dap/main.clj
+++ src/clojure_dap/main.clj
@@ -2,7 +2,9 @@
   "Entrypoint for the actual program, handles starting of systems and CLI input."
   (:require [taoensso.timbre :as log]
             [taoensso.timbre.appenders.core :as appenders]
-            [malli.core :as m]))
+            [malli.core :as m]
+            [clojure-dap.server :as server]
+            [clojure-dap.stream :as stream]))
 
 (defn run
   "CLI entrypoint to the program, boots the system and handles any CLI args."
@@ -16,10 +18,18 @@
    {:appenders {:println (appenders/println-appender {:stream :*err*})}
     :middleware [#(assoc % :hostname_ "-")]})
 
-  (log/info "Starting clojure-dap with configuration:" opts))
+  (log/info "Starting clojure-dap with configuration:" opts)
+
+  (server/start
+   {:client-io (stream/io)
+    :nrepl-io (stream/io)})
+
+  (log/info "Server started in single session mode (multi session mode will come later)")
+
+  @(promise))
 (m/=>
  run
  [:=>
   [:cat
    [:map]]
-  nil?])
+  any?])
