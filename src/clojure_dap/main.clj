f925af97b6667a43dd618946e13d3654a5c27e93
Oliver Caldwell <ollie@oli.me.uk>
2023-09-11 20:30:30 +0100

    Log EVERYTHING through timbre into stderr
    No more stdout logging and no accidental logs from slf4j/log4j etc from any libraries. EVERYTHING goes into timbre and then into stderr.

--- src/clojure_dap/main.clj
+++ src/clojure_dap/main.clj
@@ -1,13 +1,23 @@
 (ns clojure-dap.main
   "Entrypoint for the actual program, handles starting of systems and CLI input."
   (:require [clojure.pprint :as pp]
+            [taoensso.timbre :as log]
+            [taoensso.timbre.appenders.core :as appenders]
             [malli.core :as m]))
 
 (defn run
   "CLI entrypoint to the program, boots the system and handles any CLI args."
   [opts]
+  ;; Always log to stderr through timbre.
+  ;; This is because the DAP server may be communicating with a client over stdout.
+  ;; We also install extra deps that hook up essentially every other Java
+  ;; logging system to timbre, so it all goes out under stderr with the same formatting.
+  (log/merge-config!
+   {:appenders
+    {:println (appenders/println-appender {:stream :*err*})}})
+
   (pp/pprint opts)
-  (println "Hello, World!"))
+  (log/info "Hello, World!"))
 (m/=>
  run
  [:=>
