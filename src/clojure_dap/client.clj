ebc8f9d71d76a765932d9b92b48200b3308cdf44
Oliver Caldwell <ollie@oli.me.uk>
2023-09-15 17:41:00 +0100

    Write the client creation fn that maps IO to DAP

--- src/clojure_dap/client.clj
+++ src/clojure_dap/client.clj
@@ -1,2 +1,48 @@
 (ns clojure-dap.client
-  "Tools to connect a client up to a clojure-dap.io/stream-pair.")
+  "Tools to connect a client up to a clojure-dap.io/stream-pair."
+  (:require [malli.core :as m]
+            [manifold.stream :as s]
+            [manifold.deferred :as d]
+            [de.otto.nom.core :as nom]
+            [clojure-dap.stream :as stream]))
+
+(defn create
+  "Takes a client stream pair and returns another stream pair containing parsed DAP messages. When feeding it DAP messages to send it'll do the encoding for you and send them to the client."
+  [outer-io-pair]
+  (let [inner-io-pair (stream/io)
+        anomalies (s/stream stream/*stream-buffer-size*)
+        close-all! (fn []
+                     (s/close! anomalies)
+                     (stream/close-io! inner-io-pair)
+                     (stream/close-io! outer-io-pair))]
+    (d/future
+      (loop []
+        (if (or (s/closed? (:input inner-io-pair))
+                (s/closed? (:input outer-io-pair)))
+          (close-all!)
+          (let [message (stream/read-message (:input outer-io-pair))]
+            (if (nom/anomaly? message)
+              @(s/put! anomalies message)
+              @(s/put! (:input inner-io-pair) message))
+            (recur)))))
+
+    (d/future
+      (loop []
+        (if (or (s/closed? (:output inner-io-pair))
+                (s/closed? (:output outer-io-pair)))
+          (close-all!)
+          (let [message (stream/render-message @(s/take! (:output inner-io-pair)))]
+            (if (nom/anomaly? message)
+              @(s/put! anomalies message)
+              @(s/put! (:output outer-io-pair) message))
+            (recur)))))
+
+    {:client-io inner-io-pair
+     :anomalies anomalies}))
+(m/=>
+ create
+ [:=>
+  [:cat ::stream/io]
+  [:map
+   [:client-io ::stream/io]
+   [:anomalies [:fn s/stream?]]]])
--- test/clojure_dap/client_test.clj
+++ test/clojure_dap/client_test.clj
@@ -1,1 +1,59 @@
-(ns clojure-dap.client-test)
+(ns clojure-dap.client-test
+  (:require [clojure.test :as t]
+            [matcher-combinators.test]
+            [manifold.stream :as s]
+            [clojure-dap.stream :as stream]
+            [clojure-dap.client :as client]
+            [clojure-dap.stream-test :as stream-test]))
+
+(t/deftest create
+  (t/testing "connects a client IO pair to a new pair, can read and write DAP messages through it"
+    (let [outer-io (stream/io)
+          {:keys [client-io _anomalies]} (client/create outer-io)]
+
+      @(s/put! (:output client-io)
+               {:seq 153
+                :type "request"
+                :command "next"
+                :arguments {:threadId 3}})
+      (t/is
+       (= "Content-Length: 72\r\n\r\n{\"seq\":153,\"type\":\"request\",\"command\":\"next\",\"arguments\":{\"threadId\":3}}"
+          @(s/try-take! (:output outer-io) 100)))
+
+      @(s/put-all! (:input outer-io) (seq stream-test/example-message))
+      (t/is (= {:arguments {:threadId 3}
+                :command "next"
+                :seq 153
+                :type "request"}
+               @(s/try-take! (:input client-io) 100)))))
+
+  (t/testing "handles invalid messages in either direction"
+    (let [outer-io (stream/io)
+          {:keys [client-io anomalies]} (client/create outer-io)]
+
+      @(s/put! (:output client-io)
+               {:seq 153
+                :type "quest"
+                :command "next"
+                :arguments {:threadId 3}})
+      (t/is
+       (match?
+        [:de.otto.nom.core/anomaly
+         :cognitect.anomalies/incorrect
+         {:cognitect.anomalies/message
+          "Failed to validate against schema :clojure-dap.schema/message"
+          :clojure-dap.schema/explanation
+          {:schema some?
+           :value {:seq 153
+                   :type "quest"
+                   :command "next"
+                   :arguments {:threadId 3}}
+           :errors seq?}
+          :clojure-dap.schema/humanized
+          ["3 JSON Validation errors: #/type: quest is not a valid enum value, #/arguments: required key [adapterID] not found, #/command: next is not a valid enum value"
+           "3 JSON Validation errors: #: required key [request_seq] not found, #: required key [success] not found, #/type: quest is not a valid enum value"
+           "JSON Validation error: #/type: quest is not a valid enum value"
+           "3 JSON Validation errors: #: required key [request_seq] not found, #: required key [success] not found, #/type: quest is not a valid enum value"]}]
+        @(s/take! anomalies)))))
+
+  (t/testing "closes the streams if one end disconnects"))
