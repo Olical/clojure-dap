bcef596ee3de8c371cc8e697e547e891311964bc
Oliver Caldwell <ollie@oli.me.uk>
2023-10-08 14:48:22 +0100

    Add a with-thread macro that notifies us of errors

--- src/clojure_dap/util.clj
+++ src/clojure_dap/util.clj
@@ -1,2 +1,18 @@
 (ns clojure-dap.util
-  "Utility functions! Everyone's favourite namespace.")
+  "Utility functions! Everyone's favourite namespace."
+  (:require [manifold.deferred :as d]
+            [taoensso.timbre :as log]))
+
+(defmacro with-thread
+  "Create a new thread using manifold.defferred/future. Will catch any Throwable thrown inside and taoensso.timbre/error log it. The exception is then re-thrown so you can pull it out of the deferred if required."
+  [thread-name & body]
+  (assert
+   (and (keyword? thread-name) (not (simple-keyword? thread-name)))
+   "with-thread expects a namespaced keyword for a thread-name")
+
+  `(d/future
+     (try
+       ~@body
+       (catch Throwable t#
+         (log/error t# "Caught error in thread" ~thread-name)
+         (throw t#)))))
--- test/clojure_dap/util_test.clj
+++ test/clojure_dap/util_test.clj
@@ -1,5 +1,24 @@
 (ns clojure-dap.util-test
-  (:require [clojure.test :as t]))
+  (:require [clojure.test :as t]
+            [clojure-dap.util :as util]))
 
-(t/deftest placeholder
-  (t/is (= 2 (+ 1 1))))
+(t/deftest with-thread
+  (t/testing "returns a deffered we can pull a value out of"
+    (t/is (= 30 @(util/with-thread ::foo
+                   (+ 10 20)))))
+
+  (t/testing "exceptions can be pulled out of the deffered"
+    (t/is (thrown?
+           AssertionError
+           @(util/with-thread ::foo
+              (assert false)))))
+
+  (t/testing "if the exception isn't pulled out, it logs"
+    (t/is (re-find
+           #"Caught error in thread :clojure-dap.util-test/foo"
+           (with-out-str
+             (binding [*err* *out*]
+               (try
+                 @(util/with-thread ::foo
+                    (assert false))
+                 (catch AssertionError _))))))))
