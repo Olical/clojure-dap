526f0b2fa436114b9496c17c453777552518bf1a
Oliver Caldwell <ollie@oli.me.uk>
2023-10-16 21:55:33 +0100

    Starting the new main fn, not working yet but showing the outline of how it all gets plumbed together

--- src/clojure_dap/main.clj
+++ src/clojure_dap/main.clj
@@ -1,27 +1,37 @@
 (ns clojure-dap.main
   "Entrypoint for the actual program, handles starting of systems and CLI input."
-  (:require [me.raynes.fs :as rfs]
+  (:require [clojure.java.io :as io]
+            [me.raynes.fs :as rfs]
             [taoensso.timbre :as log]
             [taoensso.timbre.appenders.core :as appenders]
             [malli.experimental :as mx]
             [malli.instrument :as mi]
             [malli.dev.pretty :as malli-pretty]
-            ;; [clojure.java.io :as io]
-            ;; [manifold.stream :as s]
-            ;; [clojure-dap.server :as server]
-            ;; [clojure-dap.stream :as stream]
-            ))
+            [manifold.stream :as s]
+            [clojure-dap.util :as util]
+            [clojure-dap.server :as server]
+            [clojure-dap.stream :as stream]
+            [clojure-dap.protocol :as protocol]))
 
 (mx/defn run :- :nil
   "CLI entrypoint to the program, boots the system and handles any CLI args."
   [opts :- :map]
+
+  (.addShutdownHook
+   (Runtime/getRuntime)
+   (Thread. ^Runnable
+    (fn []
+      (log/info "Shutdown hook triggered, shutting down...")
+      (shutdown-agents)
+      (log/info "All done, goodbye!"))))
+
   (log/merge-config!
    {:appenders {;; Never log to stdout through timbre.
                 ;; This is because the DAP server may be communicating with a client over stdout.
                 ;; We also install extra deps that hook up essentially every other Java
                 ;; logging system to timbre, so it all goes out under stderr with the same formatting.
                 ;; Disabled for now, just using spit appender.
-                :println nil #_(appenders/println-appender {:stream :*err*})
+                :println (appenders/println-appender {:stream :*err*})
 
                 ;; TODO Ensure this is cross platform.
                 ;; And will we end up with multiple processes sharing the same file?
@@ -35,18 +45,25 @@
   (log/info "Initialising Malli instrumentation")
   (mi/instrument! {:report (malli-pretty/thrower)})
 
-  ;; {:reader (io/reader System/in)
-  ;;  :writer (io/writer System/out)}
+  (with-open [input-stream (s/stream)
+              output-stream (s/stream)]
 
-  (.addShutdownHook
-   (Runtime/getRuntime)
-   (Thread. ^Runnable
-    (fn []
-      (log/info "Shutdown hook triggered, shutting down...")
-      (shutdown-agents)
-      (log/info "All done, goodbye!"))))
+    (util/with-thread ::reader
+      (stream/reader-into-stream!
+       {:reader (io/reader System/in)
+        :stream input-stream}))
 
-  (log/info "Server started in single session mode (multi session mode will come later)")
+    (util/with-thread ::writer
+      (stream/stream-into-writer!
+       {:stream (s/map protocol/render-message output-stream)
+        :writer (io/writer System/out)}))
 
-  @(promise)
+    (let [server-deferred!
+          (server/run
+           {:input-stream (s/map stream/read-message input-stream)
+            :output-stream output-stream})]
+
+      (log/info "Server started in single session mode (multi session mode will come later)")
+      @server-deferred!))
+
   nil)
