c041d442f86032fd16a7e6338ac7001a09e7f94f
Oliver Caldwell <ollie@oli.me.uk>
2023-10-17 23:08:38 +0100

    Got the initial main headless version working and tested, doesn't work when connected to nvim still though...

--- src/clojure_dap/main.clj
+++ src/clojure_dap/main.clj
@@ -6,12 +6,77 @@
             [taoensso.timbre.appenders.core :as appenders]
             [malli.experimental :as mx]
             [malli.instrument :as mi]
+            [de.otto.nom.core :as nom]
             [malli.dev.pretty :as malli-pretty]
             [manifold.stream :as s]
+            [manifold.deferred :as d]
             [clojure-dap.util :as util]
             [clojure-dap.server :as server]
             [clojure-dap.stream :as stream]
             [clojure-dap.protocol :as protocol]))
+
+;; TODO Handle anomalies at all points. Needs more tests.
+(mx/defn start-server-with-io
+  :- [:map
+      [:server [:fn d/deferred?]]
+      [:anomalies-stream ::stream/stream]]
+  "Runs the server and plugs everything together with the provided reader and writer. Will handle encoding and decoding of messages into the JSON wire format.
+
+  Any anomalies from the client or the server are put into the anomalies-stream which is returned by this function alongside the server deferred."
+  [{:keys [input-reader output-writer]}
+   :- [:map
+       [:input-reader ::stream/reader]
+       [:output-writer ::stream/writer]]]
+
+  (let [input-byte-stream (s/stream)
+        input-message-stream (s/stream)
+        output-stream (s/stream)
+        anomalies-stream (s/stream)]
+
+    (util/with-thread ::reader
+      (stream/reader-into-stream!
+       {:reader input-reader
+        :stream input-byte-stream}))
+
+    (util/with-thread ::writer
+      (stream/stream-into-writer!
+       ;; TODO Refactor this mapcat pattern into something reusable if it works.
+       {:stream (->> output-stream
+                     (s/mapcat
+                      (fn [message]
+                        (let [res (protocol/render-message message)]
+                          (if (nom/anomaly? res)
+                            (do
+                              (s/put! anomalies-stream res)
+                              nil)
+                            [res])))))
+
+        :writer output-writer}))
+
+    (util/with-thread ::message-reader
+      (let [input-char-stream (s/transform
+                               (comp
+                                (filter #(>= % 1))
+                                (map char))
+                               input-byte-stream)]
+        (loop []
+          (if (s/closed? input-byte-stream)
+            (s/close! input-message-stream)
+            (do
+              (s/put! input-message-stream (stream/read-message input-char-stream))
+              (recur))))))
+
+    {:server (server/run
+              {:input-stream (s/mapcat
+                              (fn [res]
+                                (if (nom/anomaly? res)
+                                  (do
+                                    (s/put! anomalies-stream res)
+                                    nil)
+                                  [res]))
+                              input-message-stream)
+               :output-stream output-stream})
+     :anomalies-stream anomalies-stream}))
 
 (mx/defn run :- :nil
   "CLI entrypoint to the program, boots the system and handles any CLI args."
@@ -30,10 +95,9 @@
                 ;; This is because the DAP server may be communicating with a client over stdout.
                 ;; We also install extra deps that hook up essentially every other Java
                 ;; logging system to timbre, so it all goes out under stderr with the same formatting.
-                ;; Disabled for now, just using spit appender.
                 :println (appenders/println-appender {:stream :*err*})
 
-                ;; TODO Ensure this is cross platform.
+                ;; TODO Make this cross platform and not conflict with other processes.
                 ;; And will we end up with multiple processes sharing the same file?
                 ;; Also nvim users sometimes move their cache dir, so we should write to our own maybe, or tmp.
                 :spit (appenders/spit-appender {:fname (str (rfs/expand-home "~/.cache/nvim/clojure-dap.log"))})}
@@ -45,25 +109,12 @@
   (log/info "Initialising Malli instrumentation")
   (mi/instrument! {:report (malli-pretty/thrower)})
 
-  (with-open [input-stream (s/stream)
-              output-stream (s/stream)]
-
-    (util/with-thread ::reader
-      (stream/reader-into-stream!
-       {:reader (io/reader System/in)
-        :stream input-stream}))
-
-    (util/with-thread ::writer
-      (stream/stream-into-writer!
-       {:stream (s/map protocol/render-message output-stream)
-        :writer (io/writer System/out)}))
-
-    (let [server-deferred!
-          (server/run
-           {:input-stream (s/map stream/read-message input-stream)
-            :output-stream output-stream})]
-
-      (log/info "Server started in single session mode (multi session mode will come later)")
-      @server-deferred!))
+  (log/info "Starting server...")
+  (let [{:keys [server _anomalies-stream]}
+        (start-server-with-io
+         {:input-reader (io/reader System/in)
+          :output-writer (io/writer System/out)})]
+    (log/info "Server started in single session mode (multi session mode will come later)")
+    @server)
 
   nil)
---
+++ test/clojure_dap/main_test.clj
@@ -0,0 +1,42 @@
+(ns clojure-dap.main-test
+  (:require [clojure.java.io :as io]
+            [clojure.string :as str]
+            [clojure.test :as t]
+            [manifold.stream :as s]
+            [clojure-dap.protocol :as protocol]
+            [clojure-dap.main :as main]))
+
+(t/deftest start-server-with-io
+  (t/testing "responds to an initialize request appropriately"
+    (with-open [output-writer (java.io.StringWriter.)
+                input-reader (io/reader
+                              (.getBytes
+                               (protocol/render-message
+                                {:seq 1
+                                 :type "request"
+                                 :command "initialize"
+                                 :arguments {:adapterID "12345"}})))]
+      (let [{:keys [server anomalies-stream]}
+            (main/start-server-with-io
+             {:input-reader input-reader
+              :output-writer output-writer})]
+
+        @server
+
+        (s/close! anomalies-stream)
+
+        (t/is (= (str/join
+                  (map
+                   protocol/render-message
+                   [{:request_seq 1
+                     :command "initialize"
+                     :type "response"
+                     :success true
+                     :seq 1
+                     :body {:supportsConfigurationDoneRequest false
+                            :supportsCancelRequest false}}
+                    {:type "event"
+                     :event "initialized"
+                     :seq 2}]))
+                 (str output-writer)))
+        (t/is (= [] (vec (s/stream->seq anomalies-stream))))))))
