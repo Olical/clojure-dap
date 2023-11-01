bb087ecc294882982b0bab953d9f0874c7af5884
Oliver Caldwell <ollie@oli.me.uk>
2023-11-01 20:34:53 +0000

    Remove mount and portal since I don't use them at dev or runtime

--- src/giants_shoulders/repl.clj
+++ src/giants_shoulders/repl.clj
@@ -4,8 +4,6 @@
             [malli.dev :as malli-dev]
             [malli.dev.pretty :as malli-pretty]
             [taoensso.timbre :as log]
-            [portal.api :as portal]
-            [mount.core :as mount]
             [rebel-readline.core :as rr]
             [rebel-readline.clojure.line-reader :as rr-clr]
             [rebel-readline.clojure.service.local :as rr-csl]
@@ -16,23 +14,15 @@
 
 (defn start!
   "Start a development REPL, intended to be invoked from ./scripts/repl"
-  [{:keys [portal]}]
+  [_]
   (log/info "Starting malli dev instrumentation")
   (malli-dev/start! {:report (malli-pretty/reporter)})
-
-  (log/info "Starting mount system")
-  (mount/start)
 
   (log/info "Starting nREPL server")
   (let [{:keys [port] :as _server} (nrepl/start-server :handler cider/cider-nrepl-handler)]
     (log/info "nREPL server started on port" port)
     (log/info "Writing port to .nrepl-port")
     (spit ".nrepl-port" port))
-
-  (when portal
-    (log/info "Opening portal, use (tap> ...) to inspect values")
-    (portal/open)
-    (add-tap #'portal/submit))
 
   (log/info "Starting interactive REPL")
   (rr/with-line-reader
@@ -42,13 +32,6 @@
      :read (rr-cm/create-repl-read)))
 
   (log/info "Shutting down")
-
-  (when portal
-    (log/info "Closing portal")
-    (portal/close))
-
-  (log/info "Stopping mount system")
-  (mount/stop)
 
   (shutdown-agents)
   (System/exit 0))
