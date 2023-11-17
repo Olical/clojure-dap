5e459b3a8efba582756d4ef3e4b3d9e3a5756af2
Oliver Caldwell <ollie@oli.me.uk>
2023-11-17 12:18:16 +0000

    Make sure the fake debuggee supports fail? true

--- test/clojure_dap/debuggee/fake_test.clj
+++ test/clojure_dap/debuggee/fake_test.clj
@@ -1,6 +1,7 @@
 (ns clojure-dap.debuggee.fake-test
   (:require [clojure.test :as t]
             [spy.core :as spy]
+            [de.otto.nom.core :as nom]
             [clojure-dap.debuggee :as debuggee]
             [clojure-dap.debuggee.fake :as fake-debuggee]))
 
@@ -9,11 +10,17 @@
         opts {}]
     (t/is (nil? (debuggee/set-breakpoints debuggee opts)))
     (t/is (= (list (list debuggee opts))
-             (spy/calls set-breakpoints)))))
+             (spy/calls set-breakpoints))))
+
+  (let [debuggee (fake-debuggee/create {:fail? true})]
+    (t/is (nom/anomaly? (debuggee/set-breakpoints debuggee {})))))
 
 (t/deftest evaluate
   (let [{:keys [evaluate] :as debuggee} (fake-debuggee/create)
         opts {}]
     (t/is (nil? (debuggee/evaluate debuggee opts)))
     (t/is (= (list (list debuggee opts))
-             (spy/calls evaluate)))))
+             (spy/calls evaluate))))
+
+  (let [debuggee (fake-debuggee/create {:fail? true})]
+    (t/is (nom/anomaly? (debuggee/evaluate debuggee {})))))
