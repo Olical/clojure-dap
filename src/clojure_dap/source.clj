d389e36c10cc5c7f129cc6bdfb3778e8aef4adef
Oliver Caldwell <ollie@oli.me.uk>
2024-02-03 18:59:33 +0000

    Rework source tools, now adds all breakpoints then extracts all forms on their own with their break statements inside

--- src/clojure_dap/source.clj
+++ src/clojure_dap/source.clj
@@ -6,78 +6,6 @@
             [clojure.tools.reader.reader-types :as rt]
             [clojure-dap.util :as util]))
 
-(def read-opts
-  {:read-cond :preserve
-   :eof nil})
-
-(defn- with-forms
-  "Reads the forms from the source string, calling your function `f` with each form. Return ::recur to recur."
-  [source f]
-  (binding [r/*read-eval* false
-            r/*alias-map* identity]
-    (let [reader (rt/indexing-push-back-reader source)]
-      (loop []
-        (when-let [form (r/read read-opts reader)]
-          (let [result (f form)]
-            (if (= ::recur result)
-              (recur)
-              result)))))))
-
-(defn find-form-at-line
-  "Return the start and end line and column for a top level form found at the given line in the source.
-
-  Returns nil if we don't find anything or if the thing we found lacks positional metadata."
-  [{:keys [source line]}]
-  (with-forms source
-    (fn [form]
-      (let [{start-line :line
-             end-line :end-line
-             :as location}
-            (util/safe-meta form)]
-        (if (and location (<= start-line line end-line))
-          location
-          ::recur)))))
-
-(defn find-ns-form
-  "Find and return the namespace form in the given source code string, returns it's position."
-  [source]
-  (with-forms source
-    (fn [form]
-      (if (and (seq? form) (= 'ns (first form)))
-        (util/safe-meta form)
-        ::recur))))
-
-(defn extract-position
-  "Given a source string and position, extracts the string from the source denoted by the position and returns it. Also takes a lines-fn which is called with the lines after they're extracted, before they're joined. If provided, you can perform any changes to the lines before the joining happens."
-  [{:keys [source position lines-fn]}]
-  (when position
-    (let [{start-line :line
-           end-line :end-line
-           start-column :column
-           end-column :end-column}
-          position
-          source-lines (->> (line-seq (io/reader (char-array source)))
-                            (drop (dec start-line))
-                            (take (inc (- end-line start-line)))
-                            (vec))]
-      (-> source-lines
-          (update 0 subs (dec start-column))
-          (update (dec (count source-lines)) subs 0 (dec end-column))
-          (cond-> lines-fn (lines-fn))
-          (->> (str/join "\n"))))))
-
-(defn insert-break-at-line
-  "Given a position (from find-form-at-line), source string and line number, will parse out the form at the position, insert a #break statement at the line and return it.
-
-  The line number is not relative, it starts from the first line of the file."
-  [{:keys [position source line]}]
-  (extract-position
-   {:source source
-    :position position
-    :lines-fn
-    (fn [lines]
-      (update lines (- line (:line position)) #(str "#break " %)))}))
-
 (defn insert-breakpoints
   "Given a source string and a seq of breakpoints, inserts #break statements at the start of each of those lines. If the breakpoint is out of bounds, it's ignored. Lines start at 1."
   [{:keys [source breakpoints]}]
@@ -91,3 +19,51 @@
           (update idx #(str "#break " %)))))
     (str/split-lines source)
     breakpoints)))
+
+(defn extract-position
+  "Given a source string and position, extracts the string from the source denoted by the position and returns it."
+  [{:keys [source position]}]
+  (when position
+    (let [{start-line :line
+           end-line :end-line
+           start-column :column
+           end-column :end-column}
+          position
+          source-lines (->> (line-seq (io/reader (char-array source)))
+                            (drop (dec start-line))
+                            (take (inc (- end-line start-line)))
+                            (vec))
+          line-count (count source-lines)]
+      (str/join
+       "\n"
+       (if (> line-count 1)
+         (-> source-lines
+             (update 0 subs (dec start-column))
+             (update (dec line-count) subs 0 (dec end-column)))
+         (update source-lines 0 subs
+                 (dec start-column)
+                 (dec end-column)))))))
+
+(defn read-all-forms
+  "Reads all the forms from the given source string."
+  [source]
+  (binding [r/*read-eval* false
+            r/*alias-map* identity
+            r/*default-data-reader-fn* (fn [_tag value] value)]
+    (let [reader (rt/indexing-push-back-reader source)]
+      (loop [forms []]
+        (let [form (r/read
+                    {:read-cond :preserve
+                     :eof ::eof}
+                    reader)]
+          (if (= ::eof form)
+            forms
+            (let [position (util/safe-meta form)]
+              (recur
+               (if position
+                 (conj
+                  forms
+                  (extract-position
+                   {:source source
+                    :position position}))
+                 forms)))))))))
--- test/clojure_dap/source_test.clj
+++ test/clojure_dap/source_test.clj
@@ -16,92 +16,15 @@
 {::something/invalid 10
  ::another 20}")
 
-(defn line [n]
-  (source/find-form-at-line
-   {:source example-code
-    :line n}))
-
-(t/deftest find-form-at-line
-  (t/testing "reads a form at a specific line from an in memeory Clojure string"
-    (t/is (= {:column 1, :end-column 9, :end-line 1, :line 1}
-             (line 1)))
-    (t/is (nil? (line 2)))
-    (t/is (nil? (line 3)))
-    (t/is (nil? (line 4)))
-    (t/is (nil? (line 5)))
-    (t/is (= {:column 9, :end-column 11, :end-line 8, :line 6}
-             (line 6) (line 7) (line 8)))
-    (t/is (nil? (line 9)))
-    (t/is (= {:column 1, :end-column 10, :end-line 10, :line 10}
-             (line 10)))
-    (t/is (nil? (line 11)))
-    (t/is (= {:line 12, :column 1, :end-line 13, :end-column 15}
-             (line 12) (line 13)))
-    (t/is (nil? (line 14)))))
-
-(t/deftest find-ns-form
-  (t/testing "finds the namespace form in the given input code"
-    (t/is (= {:column 1, :end-column 9, :end-line 1, :line 1}
-             (source/find-ns-form example-code)))))
-
-(t/deftest extract-position
-  (t/testing "extracts a position from the given source string"
-    (t/is (= "(ns foo)"
-             (source/extract-position
-              {:position {:line 1, :column 1, :end-line 1, :end-column 9}
-               :source example-code})))))
-
-(t/deftest insert-break-at-line
-  (t/testing "inserts a #break statement at the start of the given line in the source string"
-    (t/is (= "(defn some-fn [a]\n#break   (print \"hi\")\n  (inc a))"
-             (source/insert-break-at-line
-              {:position (line 7)
-               :source example-code
-               :line 7})))
-
-    (t/is (= "#break (defn some-fn [a]\n  (print \"hi\")\n  (inc a))"
-             (source/insert-break-at-line
-              {:position (line 6)
-               :source example-code
-               :line 6})))
-
-    (t/is (= "#break (ns foo)"
-             (source/insert-break-at-line
-              {:position (line 1)
-               :source example-code
-               :line 1})))
-
-    (t/is (= "#break (+ 10 20)"
-             (source/insert-break-at-line
-              {:position (line 10)
-               :source example-code
-               :line 10})))
-
-    (t/is (= "#break {::something/invalid 10\n ::another 20}"
-             (source/insert-break-at-line
-              {:position (line 12)
-               :source example-code
-               :line 12})))
-
-    (t/is (= "{::something/invalid 10\n#break  ::another 20}"
-             (source/insert-break-at-line
-              {:position (line 13)
-               :source example-code
-               :line 13})))
-
-    (t/is (= nil
-             (source/insert-break-at-line
-              {:position nil
-               :source example-code
-               :line 7})))))
-
 (t/deftest insert-breakpoints
   (t/testing "inserts breakpoints at each of the given lines"
-    (t/is (= "#break (ns foo)\n\n10\n:foo-bar\n\n#break :before (defn some-fn [a]\n  (print \"hi\")\n  (inc a)) :after\n\n#break (+ 10 20)\n\n#break {::something/invalid 10\n ::another 20}"
+    (t/is (= "#break (ns foo)\n\n10\n:foo-bar\n\n#break :before (defn some-fn [a]\n#break   (print \"hi\")\n#break   (inc a)) :after\n\n#break (+ 10 20)\n\n#break {::something/invalid 10\n ::another 20}"
              (source/insert-breakpoints
               {:source example-code
                :breakpoints [{:line 1}
                              {:line 6}
+                             {:line 7}
+                             {:line 8}
                              {:line 10}
                              {:line 12}]}))))
 
@@ -111,3 +34,32 @@
               {:source example-code
                :breakpoints [{:line 0}
                              {:line 100}]})))))
+
+(t/deftest extract-position
+  (t/testing "extracts a position from the given source string"
+    (t/is (= "(ns foo)"
+             (source/extract-position
+              {:position {:line 1, :column 1, :end-line 1, :end-column 9}
+               :source example-code})))))
+
+(t/deftest read-all-forms
+  (t/testing "reads all forms from a source string as strings"
+    (t/is (= ["(ns foo)"
+              "(defn some-fn [a]\n  (print \"hi\")\n  (inc a))"
+              "(+ 10 20)"
+              "{::something/invalid 10\n ::another 20}"]
+             (source/read-all-forms example-code)))
+    (t/is (= [] (source/read-all-forms ""))))
+
+  (t/testing "reads with #break tags if present"
+    (t/is (= ["(ns foo)"
+              "(defn some-fn [a]\n#break   (print \"hi\")\n#break   (inc a))"
+              "(+ 10 20)"
+              "{::something/invalid 10\n ::another 20}"]
+             (source/read-all-forms
+              (source/insert-breakpoints
+               {:source example-code
+                :breakpoints [{:line 1}
+                              {:line 7}
+                              {:line 8}
+                              {:line 12}]}))))))
