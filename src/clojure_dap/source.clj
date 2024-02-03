0330343b5bc1bf0955ecb1f73fcb39c52a241813
Oliver Caldwell <ollie@oli.me.uk>
2024-02-03 17:11:55 +0000

    More refactor and code reuse

--- src/clojure_dap/source.clj
+++ src/clojure_dap/source.clj
@@ -10,35 +10,42 @@
   {:read-cond :preserve
    :eof nil})
 
+(defn- with-forms
+  "Reads the forms from the source string, calling your function `f` with each form. Return ::recur to recur."
+  [source f]
+  (binding [r/*read-eval* false
+            r/*alias-map* identity]
+    (let [reader (rt/indexing-push-back-reader source)]
+      (loop []
+        (when-let [form (r/read read-opts reader)]
+          (let [result (f form)]
+            (if (= ::recur result)
+              (recur)
+              result)))))))
+
 (defn find-form-at-line
   "Return the start and end line and column for a top level form found at the given line in the source.
 
   Returns nil if we don't find anything or if the thing we found lacks positional metadata."
   [{:keys [source line]}]
-  (binding [r/*read-eval* false
-            r/*alias-map* identity]
-    (let [reader (rt/indexing-push-back-reader source)]
-      (loop []
-        (when-let [form (r/read read-opts reader)]
-          (let [{start-line :line
-                 end-line :end-line
-                 :as location}
-                (util/safe-meta form)]
-            (if (and location (<= start-line line end-line))
-              location
-              (recur))))))))
+  (with-forms source
+    (fn [form]
+      (let [{start-line :line
+             end-line :end-line
+             :as location}
+            (util/safe-meta form)]
+        (if (and location (<= start-line line end-line))
+          location
+          ::recur)))))
 
 (defn find-ns-form
   "Find and return the namespace form in the given source code string, returns it's position."
   [source]
-  (binding [r/*read-eval* false
-            r/*alias-map* identity]
-    (let [reader (rt/indexing-push-back-reader source)]
-      (loop []
-        (when-let [form (r/read read-opts reader)]
-          (if (and (seq? form) (= 'ns (first form)))
-            (util/safe-meta form)
-            (recur)))))))
+  (with-forms source
+    (fn [form]
+      (if (and (seq? form) (= 'ns (first form)))
+        (util/safe-meta form)
+        ::recur))))
 
 (defn extract-position
   "Given a source string and position, extracts the string from the source denoted by the position and returns it. Also takes a lines-fn which is called with the lines after they're extracted, before they're joined. If provided, you can perform any changes to the lines before the joining happens."
