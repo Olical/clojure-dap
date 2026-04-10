8d72d8a8444a49d8c71f64c248d54014c53acbaf
Oliver Caldwell <ollie@oli.me.uk>
2026-04-10 11:13:48 +0100

    Add nREPL integration tests with fake nREPL server
    Introduce a configurable fake nREPL server (fake_nrepl_server.clj) that
    stubs ops (clone, eval, ls-sessions, init-debugger) with canned
    responses, enabling integration testing of the nREPL debuggee without
    a real CIDER debugger that would block on breakpoints.
    
    Tests cover:
    - create: successful connection, connection refused, port file reading,
      init-debugger producing stopped events on the output stream
    - evaluate: success with result, error output
    - threads: multiple sessions, single session
    - set-breakpoints: single/multiple breakpoints, missing source file

--- test/clojure_dap/debuggee/nrepl_test.clj
+++ test/clojure_dap/debuggee/nrepl_test.clj
@@ -1,7 +1,14 @@
 (ns clojure-dap.debuggee.nrepl-test
   (:require [clojure.test :as t]
+            [matcher-combinators.test]
+            [de.otto.nom.core :as nom]
+            [manifold.stream :as s]
+            [nrepl.server :as nrepl-server]
             [clojure-dap.protocol :as protocol]
-            [clojure-dap.debuggee.nrepl :as nrepl-debuggee]))
+            [clojure-dap.schema :as schema]
+            [clojure-dap.debuggee :as debuggee]
+            [clojure-dap.debuggee.nrepl :as nrepl-debuggee]
+            [clojure-dap.test.fake-nrepl-server :as fake-server]))
 
 (t/deftest handle-init-debugger-output
   (t/testing "returns a stopped event when status contains need-debug-input"
@@ -27,3 +34,177 @@
     (t/is (nil? (nrepl-debuggee/handle-init-debugger-output
                  {:status []
                   :session "some-session"})))))
+
+(t/deftest create
+  (t/testing "connects to an nREPL server and returns a valid debuggee"
+    (let [server (fake-server/start!)
+          output-stream (s/stream 16)]
+      (try
+        (let [debuggee (nrepl-debuggee/create
+                        {:host "127.0.0.1"
+                         :port (fake-server/port server)}
+                        {:output-stream output-stream})]
+          (t/is (not (nom/anomaly? debuggee)))
+          (t/is (nil? (schema/validate ::debuggee/debuggee debuggee)))
+          (t/is (fn? (:set-breakpoints debuggee)))
+          (t/is (fn? (:evaluate debuggee)))
+          (t/is (fn? (:threads debuggee)))
+          (t/is (fn? (:stack-trace debuggee)))
+          (t/is (fn? (:scopes debuggee)))
+          (t/is (fn? (:variables debuggee))))
+        (finally
+          (s/close! output-stream)
+          (nrepl-server/stop-server server)))))
+
+  (t/testing "returns an anomaly when connection is refused"
+    (let [output-stream (s/stream 16)]
+      (try
+        (let [result (nrepl-debuggee/create
+                      {:host "127.0.0.1"
+                       :port 1}
+                      {:output-stream output-stream})]
+          (t/is (nom/anomaly? result)))
+        (finally
+          (s/close! output-stream)))))
+
+  (t/testing "reads port from .nrepl-port file when no port specified"
+    (let [server (fake-server/start!)
+          output-stream (s/stream 16)
+          tmp-dir (System/getProperty "java.io.tmpdir")
+          port-file (java.io.File. tmp-dir ".nrepl-port")]
+      (try
+        (spit port-file (str (fake-server/port server)))
+        (let [debuggee (nrepl-debuggee/create
+                        {:root-dir tmp-dir}
+                        {:output-stream output-stream})]
+          (t/is (not (nom/anomaly? debuggee))))
+        (finally
+          (.delete port-file)
+          (s/close! output-stream)
+          (nrepl-server/stop-server server)))))
+
+  (t/testing "init-debugger with need-debug-input produces stopped event on output stream"
+    (let [server (fake-server/start!
+                  {:init-debugger [{:status #{"need-debug-input"}
+                                    :debug-value "42"
+                                    :key "debug-key"}]})
+          output-stream (s/stream 16)]
+      (try
+        (let [debuggee (nrepl-debuggee/create
+                        {:host "127.0.0.1"
+                         :port (fake-server/port server)}
+                        {:output-stream output-stream})]
+          (t/is (not (nom/anomaly? debuggee)))
+          ;; The init-debugger thread should produce a stopped event
+          ;; The session will be the fake server's session ID
+          (let [event (deref (s/take! output-stream) 2000 :timeout)]
+            (t/is (match?
+                   {:type "event"
+                    :event "stopped"
+                    :seq protocol/seq-placeholder
+                    :body {:reason "breakpoint"
+                           :threadId int?}}
+                   event))))
+        (finally
+          (s/close! output-stream)
+          (nrepl-server/stop-server server))))))
+
+(defn- with-fake-debuggee
+  "Helper: starts a fake nREPL server, creates a debuggee, calls f with it,
+  then cleans up."
+  ([f] (with-fake-debuggee {} f))
+  ([server-opts f]
+   (let [server (fake-server/start! server-opts)
+         output-stream (s/stream 16)]
+     (try
+       (let [debuggee (nrepl-debuggee/create
+                       {:host "127.0.0.1"
+                        :port (fake-server/port server)}
+                       {:output-stream output-stream})]
+         (f debuggee))
+       (finally
+         (s/close! output-stream)
+         (nrepl-server/stop-server server))))))
+
+(t/deftest evaluate-test
+  (t/testing "returns the eval result"
+    (with-fake-debuggee
+      {:eval-result "42"}
+      (fn [debuggee]
+        (let [result (debuggee/evaluate debuggee {:expression "(+ 1 2)"})]
+          (t/is (not (nom/anomaly? result)))
+          (t/is (= "42" (:result result)))))))
+
+  (t/testing "returns error output when eval fails"
+    (with-fake-debuggee
+      {:eval-error "CompilerException: unable to resolve symbol"}
+      (fn [debuggee]
+        (let [result (debuggee/evaluate debuggee {:expression "(bad-fn)"})]
+          (t/is (not (nom/anomaly? result)))
+          (t/is (= "CompilerException: unable to resolve symbol"
+                   (:result result))))))))
+
+(t/deftest threads-test
+  (t/testing "returns threads from nREPL sessions"
+    (with-fake-debuggee
+      {:sessions ["session-a" "session-b"]}
+      (fn [debuggee]
+        (let [result (debuggee/threads debuggee)]
+          (t/is (not (nom/anomaly? result)))
+          (t/is (= [{:id (hash "session-a") :name "session-a"}
+                    {:id (hash "session-b") :name "session-b"}]
+                   (:threads result)))))))
+
+  (t/testing "returns threads with single session"
+    (with-fake-debuggee
+      {:sessions ["only-session"]}
+      (fn [debuggee]
+        (let [result (debuggee/threads debuggee)]
+          (t/is (= [{:id (hash "only-session") :name "only-session"}]
+                   (:threads result))))))))
+
+(t/deftest set-breakpoints-test
+  (t/testing "instruments source and evals forms via nREPL"
+    (let [tmp-file (java.io.File/createTempFile "test" ".clj")]
+      (try
+        (spit tmp-file "(ns test-ns)\n\n(defn add [a b]\n  (+ a b))\n")
+        (with-fake-debuggee
+          {:eval-result "nil" :eval-ns "test-ns"}
+          (fn [debuggee]
+            (let [result (debuggee/set-breakpoints
+                          debuggee
+                          {:source {:path (str tmp-file)}
+                           :breakpoints [{:line 4}]})]
+              (t/is (not (nom/anomaly? result)))
+              (t/is (= {:breakpoints [{:line 4 :verified true}]}
+                       result)))))
+        (finally
+          (.delete tmp-file)))))
+
+  (t/testing "handles multiple breakpoints"
+    (let [tmp-file (java.io.File/createTempFile "test" ".clj")]
+      (try
+        (spit tmp-file "(ns test-ns)\n\n(defn add [a b]\n  (+ a b))\n\n(defn sub [a b]\n  (- a b))\n")
+        (with-fake-debuggee
+          {}
+          (fn [debuggee]
+            (let [result (debuggee/set-breakpoints
+                          debuggee
+                          {:source {:path (str tmp-file)}
+                           :breakpoints [{:line 4} {:line 7}]})]
+              (t/is (not (nom/anomaly? result)))
+              (t/is (= {:breakpoints [{:line 4 :verified true}
+                                      {:line 7 :verified true}]}
+                       result)))))
+        (finally
+          (.delete tmp-file)))))
+
+  (t/testing "returns anomaly when source file doesn't exist"
+    (with-fake-debuggee
+      {}
+      (fn [debuggee]
+        (let [result (debuggee/set-breakpoints
+                      debuggee
+                      {:source {:path "/nonexistent/file.clj"}
+                       :breakpoints [{:line 1}]})]
+          (t/is (nom/anomaly? result)))))))
---
+++ test/clojure_dap/test/fake_nrepl_server.clj
@@ -0,0 +1,91 @@
+(ns clojure-dap.test.fake-nrepl-server
+  "A fake nREPL server for integration testing. Responds to ops with
+  canned responses without actually evaluating code or triggering debugger."
+  (:require [nrepl.server :as nrepl-server]
+            [nrepl.transport :as transport]
+            [nrepl.misc :refer [response-for]]))
+
+(defn make-handler
+  "Creates a handler function for the fake nREPL server.
+  Takes an opts map that can configure behavior:
+    :eval-result    - value to return for eval ops (default \"nil\")
+    :eval-ns        - ns to return for eval ops (default \"user\")
+    :eval-error     - if set, return this as an error for eval ops
+    :sessions       - list of session IDs for ls-sessions (default [\"test-session\"])
+    :init-debugger  - seq of response maps for init-debugger op
+                      (default [{:status #{:done}}])"
+  ([] (make-handler {}))
+  ([{:keys [eval-result eval-ns eval-error sessions init-debugger]
+     :or {eval-result "nil"
+          eval-ns "user"
+          sessions ["test-session-1"]
+          init-debugger [{:status #{"done"}}]}}]
+   (let [session-counter (atom 0)]
+     (fn [{:keys [op transport] :as msg}]
+       (case op
+         "clone"
+         (let [new-session (str "fake-session-" (swap! session-counter inc))]
+           (transport/send transport
+                           (response-for msg
+                                         :new-session new-session
+                                         :status :done)))
+
+         "close"
+         (transport/send transport
+                         (response-for msg :status :done))
+
+         "describe"
+         (transport/send transport
+                         (response-for msg
+                                       :ops {"eval" {} "clone" {} "close" {}
+                                             "ls-sessions" {} "init-debugger" {}}
+                                       :status :done))
+
+         "eval"
+         (if eval-error
+           (do
+             (transport/send transport
+                             (response-for msg :err eval-error))
+             (transport/send transport
+                             (response-for msg :status #{"done" "eval-error"})))
+           (do
+             (transport/send transport
+                             (response-for msg
+                                           :value eval-result
+                                           :ns eval-ns))
+             (transport/send transport
+                             (response-for msg :status #{"done"}))))
+
+         "ls-sessions"
+         (transport/send transport
+                         (response-for msg
+                                       :sessions sessions
+                                       :status :done))
+
+         "init-debugger"
+         (doseq [response init-debugger]
+           (transport/send transport
+                           (merge
+                            (response-for msg :status (or (:status response) #{"done"}))
+                            (dissoc response :status))))
+
+         ;; Default: unknown op
+         (transport/send transport
+                         (response-for msg
+                                       :status #{:error :unknown-op :done}
+                                       :op op)))))))
+
+(defn start!
+  "Start a fake nREPL server on a random port. Returns the server.
+  Use (.getLocalPort (:server-socket server)) to get the port.
+  Stop with nrepl.server/stop-server."
+  ([] (start! {}))
+  ([opts]
+   (nrepl-server/start-server
+    :port 0
+    :handler (make-handler opts))))
+
+(defn port
+  "Get the port of a running fake nREPL server."
+  [server]
+  (.getLocalPort ^java.net.ServerSocket (:server-socket server)))
